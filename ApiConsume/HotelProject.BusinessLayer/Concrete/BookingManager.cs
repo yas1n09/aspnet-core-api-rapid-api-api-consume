530bd572a49cdde2f670e15618e91b669825fbae
yasinyagci0990@gmail.com <yasinyagci0990@gmail.com>
2024-01-18 16:00:44 +0300

    Created db table , dal , interfaces , service and the manager for booking class.

---
+++ ApiConsume/HotelProject.BusinessLayer/Concrete/BookingManager.cs
@@ -0,0 +1,46 @@
+﻿using HotelProject.BusinessLayer.Abstract;
+using HotelProject.DataAccessLayer.Abstract;
+using HotelProject.EntityLayer.Concrete;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace HotelProject.BusinessLayer.Concrete
+{
+    public class BookingManager :IBookingService
+    {
+        private readonly IBookingDal _bookingDal;
+
+        public BookingManager(IBookingDal bookingDal)
+        {
+            _bookingDal = bookingDal;
+        }
+
+        public void TDelete(Booking t)
+        {
+            _bookingDal.Delete(t);
+        }
+
+        public Booking TGetByID(int id)
+        {
+            return _bookingDal.GetByID(id);
+        }
+
+        public List<Booking> TGetLİst()
+        {
+            return _bookingDal.GetLİst();
+        }
+
+        public void TInsert(Booking t)
+        {
+            _bookingDal.Insert(t);
+        }
+
+        public void TUpdate(Booking t)
+        {
+            _bookingDal.Update(t);
+        }
+    }
+}
