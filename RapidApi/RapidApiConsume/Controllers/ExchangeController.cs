8ee9605078ad26f3985bf75d950a8103ca215a48
yasinyagci0990@gmail.com <yasinyagci0990@gmail.com>
2024-01-22 12:26:59 +0300

    RapidApi Exchange Usage

--- RapidApi/RapidApiConsume/Controllers/ExchangeController.cs
+++ RapidApi/RapidApiConsume/Controllers/ExchangeController.cs
@@ -2,32 +2,31 @@
 using Newtonsoft.Json;
 using RapidApiConsume.Models;
 using System.Net.Http.Headers;
+
 namespace RapidApiConsume.Controllers
 {
-    public class ImdbController : Controller
+    public class ExchangeController : Controller
     {
         public async Task<IActionResult> Index()
         {
-            List<ApiMovieViewModel> apiMovieViewModels = new List<ApiMovieViewModel>();
             var client = new HttpClient();
             var request = new HttpRequestMessage
             {
                 Method = HttpMethod.Get,
-                RequestUri = new Uri("https://imdb-top-100-movies.p.rapidapi.com/"),
+                RequestUri = new Uri("https://booking-com.p.rapidapi.com/v1/metadata/exchange-rates?currency=TRY&locale=tr"),
                 Headers =
     {
         { "X-RapidAPI-Key", "160a021ed4mshfc060708cb61716p12107djsnca743025e83f" },
-        { "X-RapidAPI-Host", "imdb-top-100-movies.p.rapidapi.com" },
+        { "X-RapidAPI-Host", "booking-com.p.rapidapi.com" },
     },
             };
             using (var response = await client.SendAsync(request))
             {
                 response.EnsureSuccessStatusCode();
                 var body = await response.Content.ReadAsStringAsync();
-                apiMovieViewModels=JsonConvert.DeserializeObject<List<ApiMovieViewModel>>(body);
-                return View(apiMovieViewModels);
+                var values = JsonConvert.DeserializeObject<ExchangeViewModel>(body);
+                return View(values.exchange_rates.ToList() );
             }
-            
         }
     }
 }
