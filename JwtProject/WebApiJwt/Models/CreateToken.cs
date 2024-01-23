116e1265b4e564d5d94282c679070ed2a382da5e
yasinyagci0990@gmail.com <yasinyagci0990@gmail.com>
2024-01-23 14:16:55 +0300

    -

--- JwtProject/WebApiJwt/Models/CreateToken.cs
+++ JwtProject/WebApiJwt/Models/CreateToken.cs
@@ -1,5 +1,6 @@
 ﻿using Microsoft.IdentityModel.Tokens;
 using System.IdentityModel.Tokens.Jwt;
+using System.Security.Claims;
 using System.Text;
 
 namespace WebApiJwt.Models
@@ -16,6 +17,29 @@ namespace WebApiJwt.Models
 
             JwtSecurityTokenHandler handler = new JwtSecurityTokenHandler();
             return handler.WriteToken(token);
+        }
+        public string TokenCreateAdmin()
+        {
+            var bytes = Encoding.UTF8.GetBytes("aspnetcoreapiapi");
+            SymmetricSecurityKey key = new SymmetricSecurityKey(bytes);
+            SigningCredentials credentials = new SigningCredentials (key, SecurityAlgorithms.HmacSha256);
+            List<Claim> claims = new List<Claim>()
+            {
+                new Claim(ClaimTypes.NameIdentifier,Guid.NewGuid().ToString()),
+                new Claim(ClaimTypes.Role,"Admin"),
+                new Claim(ClaimTypes.Role,"Visitor")
+            };
+            JwtSecurityToken jwtSecurityToken = new JwtSecurityToken(issuer: "http://localhost", 
+                audience: "http://localhost", 
+                notBefore: DateTime.Now, 
+                expires: DateTime.Now.AddSeconds(30),
+                signingCredentials:credentials,
+                claims:claims);
+
+            JwtSecurityTokenHandler handler= new JwtSecurityTokenHandler();
+            return handler.WriteToken(jwtSecurityToken);
+                
+            
         }
     }
 }
