bc51372f48862953873ce5b51d9dc50d401ecc33
yasinyagci0990@gmail.com <yasinyagci0990@gmail.com>
2024-01-26 14:49:02 +0300

    Created Dto Viewpage and controller for Contact. updated db

---
+++ ApiConsume/HotelProject.BusinessLayer/Concrete/ContactManager.cs
@@ -0,0 +1,46 @@
+﻿using HotelProject.BusinessLayer.Abstract;
+using HotelProject.DataAccessLayer.Abstract;
+using HotelProject.EntityLayer.Concrete;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace HotelProject.BusinessLayer.Concrete
+{
+    public class ContactManager : IContactService
+    {
+        private readonly IContactDal _contactDal;
+
+        public ContactManager(IContactDal contactDal)
+        {
+            _contactDal = contactDal;
+        }
+
+        public void TDelete(Contact t)
+        {
+            _contactDal.Delete(t);
+        }
+
+        public Contact TGetByID(int id)
+        {
+            return _contactDal.GetByID(id);
+        }
+
+        public List<Contact> TGetLİst()
+        {
+            return _contactDal.GetLİst();
+        }
+
+        public void TInsert(Contact t)
+        {
+            _contactDal.Insert(t);
+        }
+
+        public void TUpdate(Contact t)
+        {
+            _contactDal.Update(t);
+        }
+    }
+}
