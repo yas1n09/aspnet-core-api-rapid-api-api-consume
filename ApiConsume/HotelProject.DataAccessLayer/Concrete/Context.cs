da5bafb39d1b3a1c2f7f90d652c2c10148898160
yasinyagci0990@gmail.com <yasinyagci0990@gmail.com>
2024-01-29 13:56:56 +0300

    Created dtos , Created add-update-list room pages.

--- ApiConsume/HotelProject.DataAccessLayer/Concrete/Context.cs
+++ ApiConsume/HotelProject.DataAccessLayer/Concrete/Context.cs
@@ -15,6 +15,35 @@ namespace HotelProject.DataAccessLayer.Concrete
         {
             optionsBuilder.UseSqlServer("Server=(LocalDb)\\MSSQLLocalDB;initial catalog=ApiDB; Integrated Security=true;");
         }
+
+        protected override void OnModelCreating(ModelBuilder builder)
+        {
+            base.OnModelCreating(builder);
+
+            builder.Entity<Room>(entry =>
+            {
+                entry.ToTable("Rooms", tb => tb.HasTrigger("RoomIncrease"));
+                entry.ToTable("Rooms", tb => tb.HasTrigger("RoomDecrease"));
+            });
+            builder.Entity<Staff>(entry =>
+            {
+                entry.ToTable("Staffs", tb => tb.HasTrigger("StaffIncrease"));
+                entry.ToTable("Staffs", tb => tb.HasTrigger("StaffDecrease"));
+            });
+            builder.Entity<Guest>(entry =>
+            {
+                entry.ToTable("Guests", tb => tb.HasTrigger("GuestIncrease"));
+                entry.ToTable("Guests", tb => tb.HasTrigger("GuestDecrease"));
+            });
+
+            //builder.Entity<Room>().HasKey(r => r.RoomID);
+            //builder.Entity<Service>().HasKey(s => s.ServiceID);
+            //builder.Entity<Staff>().HasKey(st => st.StaffID);
+            //builder.Entity<Subscribe>().HasKey(su => su.SubscribeID);
+            //builder.Entity<Testimonial>().HasKey(t => t.TestimonialID);
+
+        }
+
         public DbSet<Room> Rooms { get; set; }
         public DbSet<Service> Services { get; set; }
         public DbSet<Staff> Staffs { get; set; }
