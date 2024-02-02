75bae6d1ab65c713a1dae965443a1c8ef3daea3c
yasinyagci0990@gmail.com <yasinyagci0990@gmail.com>
2024-02-02 16:01:47 +0300

    Created Interfaces And the other stuff for send message

---
+++ ApiConsume/HotelProject.BusinessLayer/Concrete/SendMessageManager.cs
@@ -0,0 +1,46 @@
+﻿using HotelProject.BusinessLayer.Abstract;
+using HotelProject.DataAccessLayer.Abstract;
+using HotelProject.EntityLayer.Concrete;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace HotelProject.BusinessLayer.Concrete
+{
+    public class SendMessageManager : ISendMessageService
+    {
+        private readonly ISendMessageDal _sendMessageDal;
+
+        public SendMessageManager(ISendMessageDal sendMessageDal)
+        {
+            _sendMessageDal = sendMessageDal;
+        }
+
+        public void TDelete(SendMessage entity)
+        {
+            _sendMessageDal.Delete(entity);
+        }
+
+        public SendMessage TGetByID(int id)
+        {
+            return _sendMessageDal.GetByID(id);
+        }
+
+        public List<SendMessage> TGetLİst()
+        {
+            return _sendMessageDal.GetLİst();
+        }
+
+        public void TInsert(SendMessage entity)
+        {
+            _sendMessageDal.Insert(entity);
+        }
+
+        public void TUpdate(SendMessage entity)
+        {
+            _sendMessageDal.Update(entity);
+        }
+    }
+}
--- Frontend/HotelProject.WebUI/Controllers/AdminContactController.cs
+++ Frontend/HotelProject.WebUI/Controllers/AdminContactController.cs
@@ -1,13 +1,32 @@
-﻿using Microsoft.AspNetCore.Mvc;
+﻿using HotelProject.WebUI.Dtos.ContactDto;
+using HotelProject.WebUI.Dtos.StaffDto;
+using Microsoft.AspNetCore.Mvc;
+using Newtonsoft.Json;
 
 namespace HotelProject.WebUI.Controllers
 {
     public class AdminContactController : Controller
     {
-        public IActionResult Inbox()
+        private readonly IHttpClientFactory _httpClientFactory;
+
+        public AdminContactController(IHttpClientFactory httpClientFactory)
         {
+            _httpClientFactory = httpClientFactory;
+        }
+
+        public async Task<IActionResult> Inbox()
+        {
+            var client = _httpClientFactory.CreateClient();
+            var responseMessage = await client.GetAsync("http://localhost:49452/api/Contact");
+            if (responseMessage.IsSuccessStatusCode)
+            {
+                var jsonData = await responseMessage.Content.ReadAsStringAsync();
+                var values = JsonConvert.DeserializeObject<List<InboxContactDto>>(jsonData);
+                return View(values);
+            }
             return View();
         }
+        
         public PartialViewResult SideBarAdminContactPartial()
         {
             return PartialView();
