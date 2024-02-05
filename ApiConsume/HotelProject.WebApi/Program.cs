62a701c2e6a426aba01d332dd518159bd488889a
yasinyagci0990@gmail.com <yasinyagci0990@gmail.com>
2024-02-05 15:15:41 +0300

    SendMessage working with api correctly

--- ApiConsume/HotelProject.WebApi/Program.cs
+++ ApiConsume/HotelProject.WebApi/Program.cs
@@ -45,6 +45,9 @@ builder.Services.AddScoped<IContactService, ContactManager>();
 builder.Services.AddScoped<IGuestDal, EfGuestDal>();
 builder.Services.AddScoped<IGuestService, GuestManager>();
 
+builder.Services.AddScoped<ISendMessageDal, EfSendMessageDal>();
+builder.Services.AddScoped<ISendMessageService, SendMessageManager>();
+
 
 
 builder.Services.AddAutoMapper(typeof(Program));
--- Frontend/HotelProject.WebUI/Controllers/AdminContactController.cs
+++ Frontend/HotelProject.WebUI/Controllers/AdminContactController.cs
@@ -35,14 +35,13 @@ namespace HotelProject.WebUI.Controllers
             return View();
         }
         [HttpPost]
-        public async Task<IActionResult> AddSendMessage(CreateSendMessageDto createSendMessageDto)
-        {
-            if (!ModelState.IsValid)
-            {
-                return View();
-            }
+        public async Task<IActionResult> AddSendMessage(CreateSendMessageDto createSendMessage)
+        {           
+            createSendMessage.SenderMail = "admin@gmail.com";
+            createSendMessage.SenderName = "admin";
+            createSendMessage.Date = DateTime.Parse(DateTime.Now.ToShortDateString());
             var client = _httpClientFactory.CreateClient();
-            var jsonData = JsonConvert.SerializeObject(createSendMessageDto);
+            var jsonData = JsonConvert.SerializeObject(createSendMessage);
             StringContent stringContent = new StringContent(jsonData, Encoding.UTF8, "application/json");
             var responseMessage = await client.PostAsync("http://localhost:49452/api/SendMessage", stringContent);
 
